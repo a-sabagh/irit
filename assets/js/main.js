c0a6551310e8233b8bfd9079f9380c67f7d119c9
asabagh <a.sabagh72@gmail.com>
2018-11-30 18:44:19 +0330

    section one

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function ($) {
+    $(".full-slider").slick({
+        rtl: true,
+        prevArrow: '<span class="slider-arrow arrow-prev fa fa-angle-left"></span>',
+        nextArrow: '<span class="slider-arrow arrow-next fa fa-angle-right"></span>'
+    });
+});
