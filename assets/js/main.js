670c30a19a659a75f56228d9e19c1661daa506e0
asabagh <a.sabagh72@gmail.com>
2018-12-01 23:56:56 +0330

    section 2 11:56

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,4 +4,22 @@ jQuery(document).ready(function ($) {
         prevArrow: '<span class="slider-arrow arrow-prev fa fa-angle-left"></span>',
         nextArrow: '<span class="slider-arrow arrow-next fa fa-angle-right"></span>'
     });
+    //featured-tab
+//    $(".featured-tab-container .tab-menu-content").hide(); //hide all contenet of tab
+//    $("ul.featured-head li:first").addClass("active"); //show first tab is active
+//    $(".featured-tab-container .tab-menu-content:first").show(); //show first content is of tab
+//    $("ul.featured-head li").click(function () {
+//        var child_count = $(this).lenght;
+//        console.log(child_count);
+//
+//        $(".featured-tab-container .tab-menu-content").hide(); //hide all contenet of tab
+//        $("ul.featured-head li").removeClass("active"); //remove all active class
+//
+//        $(this).addClass("active"); //active the tab that you click this
+//        $(this).parent("ul")
+//            .siblings(".featured-tab-container")
+//            .find(".tab-menu-content")[child_count]
+//            .fadeIn(); //fade in the content of tab you click on this
+//        return false;
+//    });
 });
