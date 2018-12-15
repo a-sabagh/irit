5d3141718f6b868498f3e214ed1de732aedd6480
asabagh <a.sabagh72@gmail.com>
2018-12-15 17:05:59 +0330

    irtt setup configuration

--- includes/init.php
+++ includes/init.php
@@ -6,11 +6,14 @@ if (!function_exists("irtt_setup")) {
         add_theme_support("title-tag");
         add_theme_support("post-thumbnails");
         add_theme_support('html5', array('comment-list', 'comment-form', 'search-form', 'gallery', 'caption'));
-
         add_theme_support('custom-header');
         add_theme_support("post-formats", array("video"));
         add_theme_support("menus");
 
+        //localization
+        load_theme_textdomain("irtt", get_template_directory() . "/languages");
+
+        //add image template size
         add_image_size("thumb1", 240, 230);
         add_image_size("thumb2", 240, 230);
         add_image_size("thumb3", 240, 230);
@@ -20,10 +23,10 @@ if (!function_exists("irtt_setup")) {
 }
 if (!function_exists("irtt_load_text_domain")) {
 
-    function rudy_load_text_domain() {
-        load_theme_textdomain("irtt", get_template_directory() . "/languages");
+    function irtt_load_text_domain() {
+        
     }
 
 }
-add_action("init", "irtt_load_text_domain");
+
 add_action("after_setup_theme", "irtt_setup");
