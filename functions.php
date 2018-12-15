5ed86d08f22d7b6c9ce392e6d101601f6ea840ae
asabagh <a.sabagh72@gmail.com>
2018-12-15 11:05:44 +0330

    init wordpress theme

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php 
+define("IRTT_FRONT", get_template_directory_uri());
+define("IRTT_ADMIN" , get_template_directory_uri() . "/admin/templates" );
+
+
+require_once 'includes/init.php';
+require_once 'includes/scripts.php';
+require_once 'includes/functions.php';
---
+++ html-static/assets/js/main.js
@@ -0,0 +1,109 @@
+jQuery(document).ready(function ($) {
+    if ($(".full-slider").length) {
+        $(".full-slider").slick({
+            rtl: true,
+            prevArrow: '<span class="slider-arrow arrow-prev fa fa-angle-left"></span>',
+            nextArrow: '<span class="slider-arrow arrow-next fa fa-angle-right"></span>'
+        });
+    }
+    //featured-tab
+    if ($(".featured-tab-container").length) {
+        $(".featured-tab-container").each(function () {
+            $(this).find(".featured-content .tab-menu-content").hide(); //hide all contenet of tab
+            $(this).find(".featured-head li:first").addClass("active"); //show first tab is active
+            $(this).find(".featured-content .tab-menu-content").first().show(); //show first content is of tab
+            $(this).find(".featured-head li").click(function (e) {
+                e.preventDefault();
+                var child_count = $(this).index();
+                $(this)
+                    .parents(".featured-head")
+                    .siblings(".featured-content")
+                    .children(".tab-menu-content")
+                    .hide(); //hide all contenet of tab
+                $(this)
+                    .parents(".featured-head")
+                    .find("li")
+                    .removeClass("active"); //remove all active class
+
+                $(this).addClass("active"); //active the tab that you click this
+                $(this).parents(".featured-head")
+                    .siblings(".featured-content")
+                    .find(".tab-menu-content:nth-child(" + (child_count + 1) + ")")
+                    .fadeIn(); //fade in the content of tab you click on this
+                return false;
+            });
+        });
+    }
+
+    //sale-product-slider
+    var saleSliderProduct = $(".sale-slider-product");
+    if (saleSliderProduct.length) {
+        $(".sale-slider-product").slick({
+            rtl: true,
+            centerMode: true,
+            centerPadding: '0px',
+            prevArrow: '<span class="slider-arrow arrow-prev fa fa-angle-left multiple"></span>',
+            nextArrow: '<span class="slider-arrow arrow-next fa fa-angle-right multiple"></span>',
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 5000,
+            infinite: true,
+        });
+    }
+    //slick-multiple-simple
+    var sliderMultipleSimple = $(".slider-multiple-simple");
+    if (sliderMultipleSimple.length) {
+        sliderMultipleSimple.slick({
+            rtl: true,
+            centerMode: true,
+            centerPadding: '0px',
+            prevArrow: '<span class="slider-arrow arrow-prev fa fa-angle-left multiple"></span>',
+            nextArrow: '<span class="slider-arrow arrow-next fa fa-angle-right multiple"></span>',
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 5000,
+            infinite: true,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                        infinite: true,
+                    }
+			},
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2
+                    }
+			},
+                {
+                    breakpoint: 550,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+			}
+		]
+        });
+    }
+    //home-video-dom
+    $(".home-video .play-icon").on('click', function (e) {
+        e.preventDefault();
+        $("#home-video-dom").get(0).play();
+        $(".home-video .play-icon").hide();
+        $(".home-video h3").hide();
+    });
+    //responsive menu
+    $(".menu-item-has-children .menu-plus").on('click', function () {
+        $(this).toggleClass("open");
+        $(this).next("ul").slideToggle('normal');
+    });
+    $(".navigation-wrapper .menu-toggler").on('click', function () {
+        $(this).next("#main-menu").slideToggle();
+    });
+});
