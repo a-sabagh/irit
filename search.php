2f05603bdaa78f2f0b8f2b8c635c0b4d2129d77b
asabagh <a.sabagh72@gmail.com>
2018-12-20 12:20:11 +0330

    search template

---
+++ search.php
@@ -0,0 +1,33 @@
+<?php
+get_header();
+get_template_part('templates/headers/search');
+?>
+<main class="container">
+    <div class="single-main-content row">
+        <div class="col-md-8">
+            <?php
+            if (have_posts()) {
+                while (have_posts()) {
+                    the_post();
+                    get_template_part('templates/loops/archive');
+                }
+            } else {
+                get_template_part('templates/loops/no','result');
+            }
+            ?>
+            <ul class="pagination pagination-border">
+                <li><a href="#">1</a></li>
+                <li><a class="active" href="#">2</a></li>
+                <li><a href="#">3</a></li>
+                <li><a href="#">4</a></li>
+                <li><a href="#">5</a></li>
+                <li><a href="#">6</a></li>
+                <li><a href="#">7</a></li>
+            </ul>
+        </div>
+        <!--.col-md-8-->
+        <?php get_sidebar(); ?>
+    </div>
+</main>
+<!--.single-main-content-->
+<?php get_footer(); ?>
---
+++ templates/headers/search.php
@@ -0,0 +1,4 @@
+<header class="page-header">
+    <h1 class="page-title container"><?php echo sprintf(__('جستجو بر اساس عبارت %s'), $s); ?></h1>
+</header>
+<!--.page-header-->
