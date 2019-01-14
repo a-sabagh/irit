c1e02e12e2da82bf461456069e3158a85e33344c
asabagh <a.sabagh72@gmail.com>
2019-01-14 11:00:17 +0330

    make section-three 4 banner

--- admin/templates/metaboxes/section-three.php
+++ admin/templates/metaboxes/section-three.php
@@ -3,7 +3,7 @@
 
     <td colspan="2">
         <p>
-            <span><?php _e('بنر های میانی برگه . بالای ویدئو (دو عدد)', 'irtt'); ?></span>
+            <span><?php _e('بنر های میانی برگه . بالای ویدئو (چهار عدد)', 'irtt'); ?></span>
             <button class="modal-button button" data-modal="section-three-banners"><?php _e('ویرایش', 'irtt'); ?></button>
         </p>
         <!-- The Modal -->
@@ -18,7 +18,7 @@
                 <div class="modal-body">
                     <div class="splite-columns">
                         <div class="splite-columns-item">
-                            <p><?php _e('بنر سمت راست','irtt') ?></p>
+                            <p><?php _e('بنر اول','irtt') ?></p>
                             <div class="rngwp-btn-wrapper">
                                 <input type="hidden" class="rng-link-banner2" name="irtt_home_template[b3][1][src]" value="<?php echo $home_template['b3'][1]['src']; ?>">
                                 <button class="rng-button-banner2 wp-core-ui button rng-btn-select"><?php _e('افزودن فایل', 'irtt') ?></button>
@@ -27,13 +27,31 @@
                             <input style="width: 100%" type="text" name="irtt_home_template[b3][1][link]" value="<?php echo $home_template['b3'][1]['link']; ?>" >
                         </div>
                         <div class="splite-columns-item">
-                            <p><?php _e('بنر سمت چپ','irtt') ?></p>
+                            <p><?php _e('بنر دوم','irtt') ?></p>
                             <div class="rngwp-btn-wrapper">
                                 <input type="hidden" class="rng-link-banner2" name="irtt_home_template[b3][2][src]" value="<?php echo $home_template['b3'][2]['src']; ?>">
                                 <button class="rng-button-banner2 wp-core-ui button rng-btn-select"><?php _e('افزودن فایل', 'irtt') ?></button>
                                 <button class="wp-core-ui button rng-btn-reset2" <?php echo (empty($home_template['b3'][2]['src'])) ? "disabled" : ""; ?>><?php _e('پاک کردن فایل', 'irtt') ?></button>
                             </div>
                             <input style="width: 100%" type="text" name="irtt_home_template[b3][2][link]" value="<?php echo $home_template['b3'][2]['link']; ?>" >
+                        </div>
+                        <div class="splite-columns-item">
+                            <p><?php _e('بنر سوم','irtt') ?></p>
+                            <div class="rngwp-btn-wrapper">
+                                <input type="hidden" class="rng-link-banner2" name="irtt_home_template[b3][3][src]" value="<?php echo $home_template['b3'][3]['src']; ?>">
+                                <button class="rng-button-banner2 wp-core-ui button rng-btn-select"><?php _e('افزودن فایل', 'irtt') ?></button>
+                                <button class="wp-core-ui button rng-btn-reset2" <?php echo (empty($home_template['b3'][3]['src'])) ? "disabled" : ""; ?>><?php _e('پاک کردن فایل', 'irtt') ?></button>
+                            </div>
+                            <input style="width: 100%" type="text" name="irtt_home_template[b3][3][link]" value="<?php echo $home_template['b3'][3]['link']; ?>" >
+                        </div>
+                        <div class="splite-columns-item">
+                            <p><?php _e('بنر چهارم','irtt') ?></p>
+                            <div class="rngwp-btn-wrapper">
+                                <input type="hidden" class="rng-link-banner2" name="irtt_home_template[b3][4][src]" value="<?php echo $home_template['b3'][4]['src']; ?>">
+                                <button class="rng-button-banner2 wp-core-ui button rng-btn-select"><?php _e('افزودن فایل', 'irtt') ?></button>
+                                <button class="wp-core-ui button rng-btn-reset2" <?php echo (empty($home_template['b3'][4]['src'])) ? "disabled" : ""; ?>><?php _e('پاک کردن فایل', 'irtt') ?></button>
+                            </div>
+                            <input style="width: 100%" type="text" name="irtt_home_template[b3][4][link]" value="<?php echo $home_template['b3'][4]['link']; ?>" >
                         </div>
                     </div>
                 </div>
