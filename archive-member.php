ad13996cda925cda24e059072c1dd505e07bb5f3
asabagh <a.sabagh72@gmail.com>
2019-06-27 13:40:27 +0430

    add home new version and archive-member files

---
+++ archive-member.php
@@ -0,0 +1,371 @@
+<!DOCTYPE html>
+<html lang="fa-IR" dir="rtl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="assets/css/reset.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/slick.css">
+    <link rel="stylesheet" href="assets/css/font-awesome.css">
+	<link rel="stylesheet" href="assets/css/main.css" >
+	<link rel="stylesheet" href="assets/css/master.css" >
+    <title>صفحه اصلی - اندیشکده روابط بین الملل</title>
+
+</head>
+<body>
+	<header id="header">
+		<section class="top-header">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-4">
+						<ul class="header-top-social text-center text-lg-right">
+							<li>
+								<a href="#" title="">
+									<i class="fa fa-facebook"></i>
+								</a>
+							</li>
+							<li>
+								<a href="#" title="">
+									<i class="fa fa-instagram"></i>
+								</a>
+							</li>
+							<li>
+								<a href="#" title="">
+									<i class="fa fa-telegram"></i>
+								</a>
+							</li>
+							<li>
+								<a href="#" title="">
+									<i class="fa fa-linkedin"></i>
+								</a>
+							</li>
+							<li>
+								<a href="#" title="">
+									<i class="fa fa-twitter"></i>
+								</a>
+							</li>
+							<li>
+								<a href="#" title="">
+									<i class="fa fa-youtube	"></i>
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div class="col-md-lg d-flex justify-content-xl-end justify-content-center">
+						<ul class="header-top-menu">
+							<li><a href="#">تماس با ما</a></li>
+							<li><a href="#">درباره ما</a></li>
+							<li><a href="#">قوانین و مقررات</a></li>
+							<li><a href="#">فروشگاه</a></li>
+							<li><a href="#">سبد خرید</a></li>
+						</ul>
+					</div>
+				</div><!--.row-->
+			</div><!--.container-->
+		</section><!--.top-header-->
+		<section class="bottom-header">
+			<div class="container">
+				<div class="d-flex flex-nowrap justify-content-xl-start justify-content-between align-items-center">
+					<a href="#" title="اندیشکده بین الملل" >
+						<img class="img-responsive logo" src="images/logo.png" alt="">
+					</a>
+					<nav class="navigation-wrapper">
+						<a href="#" class="menu-toggler" title="منو"><span>منوی اصلی</span><i class="fa fa-bars"></i></a>
+						<ul id="main-menu">
+							<li class="active"><a href="#">صفحه اصلی</a></li>
+							<li class="menu-item-has-children">
+								<a href="">دوره ها</a>
+								<span class="menu-plus"></span>
+								<ul>
+									<li><a href="#">سطح دوم</a></li>
+									<li class="menu-item-has-children">
+										<a href="#">مهارت های ICDL</a>
+										<span class="menu-plus"></span>
+										<ul>
+											<li>
+                                                <a href="#">مهارت دوم مایکروسافت آفیس</a>
+                                                <ul>
+                                                    <li><a href="#">مهارت دوم مایکروسافت آفیس</a></li>
+                                                    <li><a href="#">مهارت سوم مایکروسافت اکسل</a></li>
+                                                    <li><a href="#">مهارت اول آشنایی با فناوری اطلاعات</a></li>
+                                                </ul>
+                                            </li>
+											<li><a href="#">مهارت سوم مایکروسافت اکسل</a></li>
+											<li><a href="#">مهارت اول آشنایی با فناوری اطلاعات</a></li>
+										</ul>
+									</li>
+									<li><a href="#">سطح دوم</a></li>
+									<li><a href="#">سطح دوم</a></li>
+									<li><a href="#">سطح دوم</a></li>
+								</ul>
+							</li>
+							<li><a href="#">ویدئوی آموزشی</a></li>
+							<li><a href="#">تماس با من</a></li>
+							<li><a href="#">اقتصاد بین الملل</a></li>
+							<li><a href="#">اخبار آسیا</a></li>
+							<li><a href="#">آمریکای جنوبی</a></li>
+							<li><a href="#">درباره من</a></li>
+						</ul>
+					</nav>
+					<!--.navigation-wrapper-->
+				</div>
+			</div>
+		</section><!--.bottom-header-->
+	</header><!--#header-->
+    <main id="main">
+        <div class="container">
+            <div class="row  archive-person-contnet">
+                <div class="col-md-8">
+                    <div class="d-flex justify-content-between flex-wrap">
+                        <h2 class="section-title">پژوهشگران و نویسندگان</h2>
+                        <div class="search-person-wrapper">
+                            <form action="" method="get">
+                                <input type="text" placeholder="جستجو در میان پژوهشگران" name="person-search">
+                                <input type="submit" value="">
+                            </form>
+                        </div>
+                    </div>
+                    <div class="person-wrapper d-flex align-items-center justify-content-center flex-wrap">
+                        <a href="#" title="" class="person-item">
+                            <div class="img-wrapper text-center">
+                                <img class="img-responsive" src="images/personal.jpg" alt="">
+                            </div>
+                            <p class="bio">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.</p>
+                        </a><!--.person-item-->
+                        <a href="#" title="" class="person-item">
+                            <div class="img-wrapper text-center">
+                                <img class="img-responsive" src="images/personal.jpg" alt="">
+                            </div>
+                            <p class="bio">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.</p>
+                        </a><!--.person-item-->
+                        <a href="#" title="" class="person-item">
+                            <div class="img-wrapper text-center">
+                                <img class="img-responsive" src="images/personal.jpg" alt="">
+                            </div>
+                            <p class="bio">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.</p>
+                        </a><!--.person-item-->
+                        <a href="#" title="" class="person-item">
+                            <div class="img-wrapper text-center">
+                                <img class="img-responsive" src="images/personal.jpg" alt="">
+                            </div>
+                            <p class="bio">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.</p>
+                        </a><!--.person-item-->
+                        <a href="#" title="" class="person-item">
+                            <div class="img-wrapper text-center">
+                                <img class="img-responsive" src="images/personal.jpg" alt="">
+                            </div>
+                            <p class="bio">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.</p>
+                        </a><!--.person-item-->
+                        <a href="#" title="" class="person-item">
+                            <div class="img-wrapper text-center">
+                                <img class="img-responsive" src="images/personal.jpg" alt="">
+                            </div>
+                            <p class="bio">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.</p>
+                        </a><!--.person-item-->
+                        <a href="#" title="" class="person-item">
+                            <div class="img-wrapper text-center">
+                                <img class="img-responsive" src="images/personal.jpg" alt="">
+                            </div>
+                            <p class="bio">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.</p>
+                        </a><!--.person-item-->
+                        <a href="#" title="" class="person-item">
+                            <div class="img-wrapper text-center">
+                                <img class="img-responsive" src="images/personal.jpg" alt="">
+                            </div>
+                            <p class="bio">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.</p>
+                        </a><!--.person-item-->
+                        <a href="#" title="" class="person-item">
+                            <div class="img-wrapper text-center">
+                                <img class="img-responsive" src="images/personal.jpg" alt="">
+                            </div>
+                            <p class="bio">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.</p>
+                        </a><!--.person-item-->
+                    </div><!--.person-wrapper-->
+                </div><!--.main-content-->
+                <div class="col-md-4 person-static-side">
+                    <div class="mail-poet-wrapper">
+                        <header class="d-flex justify-content-start align-items-center flex-nowrap">
+                            <i class="fa fa-envelope-open-o"></i>
+                            <div class="text-wrapper">
+                                <p class="medium-text">عضویت در خبرنامه</p>
+                                <p class="small-text">با عضویت در خبر نامه از جدیدترین اخبار و تخفیف های ما آگاه شوید.</p>
+                            </div>
+                        </header>
+                        <div class="form-wrapper">
+                            <form action="" method="post">
+                                <input type="text" name="name" value="" placeholder="نام کامل">
+                                <input type="text" name="email" value="" placeholder="ایمیل">
+                                <input type="submit" value="عضویت در خبرنامه">
+                            </form>
+                        </div>
+                    </div><!--.mail-poet-wrapper-->
+                    <div class="special-sign-in-wrapper">
+                        <header>
+                            <div><i class="fa fa-users"></i></div>
+                            <p>مزایای عضویت در اندیشکده</p>
+                        </header>
+                        <div class="content" >
+                            <ul>
+                                <li>دریافت تحلیل های خاورمیانه</li>
+                                <li>اقتصاد بین الملل</li>
+                                <li>کتاب ها و وقایع</li>
+                                <li>مشاوره تخصصی</li>
+                            </ul>
+                        </div>
+                    </div><!--.special-sign-in-wrapper-->
+                    <div class="last-news-wrapper">
+                        <ul>
+                            <li class="d-flex justify-content-between align-items-center">
+                                <a href="#" class="img-wrapper"><img src="images/thumb.jpg" alt="" class="img-responsive"></a>
+                                <div class="content-wrapper">
+                                    <a href="#">
+                                        <h4>تحریم های جدید آمریکا علیه ایران</h4>
+                                        <p class="content">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ</p>
+                                    </a>
+                                </div>
+                            </li>
+                            <li class="d-flex justify-content-between align-items-center">
+                                <a href="#" class="img-wrapper"><img src="images/thumb.jpg" alt="" class="img-responsive"></a>
+                                <div class="content-wrapper">
+                                    <a href="#">
+                                        <h4>تحریم های جدید آمریکا علیه ایران</h4>
+                                        <p class="content">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ</p>
+                                    </a>
+                                </div>
+                            </li>
+                            <li class="d-flex justify-content-between align-items-center">
+                                <a href="#" class="img-wrapper"><img src="images/thumb.jpg" alt="" class="img-responsive"></a>
+                                <div class="content-wrapper">
+                                    <a href="#">
+                                        <h4>تحریم های جدید آمریکا علیه ایران</h4>
+                                        <p class="content">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ</p>
+                                    </a>
+                                </div>
+                            </li>
+                            <li class="d-flex justify-content-between align-items-center">
+                                <a href="#" class="img-wrapper"><img src="images/thumb.jpg" alt="" class="img-responsive"></a>
+                                <div class="content-wrapper">
+                                    <a href="#">
+                                        <h4>تحریم های جدید آمریکا علیه ایران</h4>
+                                        <p class="content">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ</p>
+                                    </a>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div><!--.person-static-side-->
+            </div>
+        </div>
+    </main><!--#main-->
+    <footer id="footer">
+		<section class="top-footer">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4 contact-info">
+						<h3 class="widg-title">اطلاعات تماس</h3>
+						<ul class="contact-value">
+							<li>
+								<i class="fa fa-map"></i>
+								<span>تهران، جردن، خیابان نلسون ماندلا، نبش خیابان طاهری، برج مشکی، طبقه 8، واحد 1</span>
+							</li>
+							<li>
+								<i class="fa fa-phone"></i>
+                                <span class="font-english">09125645463</span>
+							</li>
+							<li>
+								<i class="fa fa-envelope"></i>
+                                <span>irtt.ir@gmail.com</span>
+							</li>
+						</ul>
+                        <ul class="footer-social">
+							<li>
+								<a href="#" title="">
+									<i class="fa fa-facebook"></i>
+								</a>
+							</li>
+							<li>
+								<a href="#" title="">
+									<i class="fa fa-instagram"></i>
+								</a>
+							</li>
+							<li>
+								<a href="#" title="">
+									<i class="fa fa-telegram"></i>
+								</a>
+							</li>
+							<li>
+								<a href="#" title="">
+									<i class="fa fa-linkedin"></i>
+								</a>
+							</li>
+							<li>
+								<a href="#" title="">
+									<i class="fa fa-twitter"></i>
+								</a>
+							</li>
+							<li>
+								<a href="#" title="">
+									<i class="fa fa-youtube	"></i>
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div class="col-md-4 d-flex justify-content-around">
+						<div class="widg-wrapper">
+							<h3 class="widg-title">دسترسی سریع</h3>
+                            <div class="widg-content">
+                                <ul>
+                                    <li><a href="#">کتابخانه</a></li>
+                                    <li><a href="#">یادداشت</a></li>
+                                    <li><a href="#">غرب آسیا</a></li>
+                                    <li><a href="#">بیداری اسلامی</a></li>
+                                </ul>
+                            </div>
+						</div><!--.widg-wrapper-->
+						<div class="widg-wrapper">
+							<h3 class="widg-title">دسترسی سریع</h3>
+                            <div class="widg-content">
+                                <ul>
+                                    <li><a href="#">کتابخانه</a></li>
+                                    <li><a href="#">یادداشت</a></li>
+                                    <li><a href="#">غرب آسیا</a></li>
+                                    <li><a href="#">بیداری اسلامی</a></li>
+                                </ul>
+                            </div>
+						</div><!--.widg-wrapper-->
+					</div><!--.col-md-4-->
+
+					<div class="col-md-4 footer-widg">
+						<h3 class="widg-title">اپلیکیشن و خبرنامه</h3>
+						<ul class="applications">
+							<li class="android" >
+								<a title="" href="#">
+									<i class="fa fa-android"></i>
+									<span>اپلیکیشن اندروید</span>
+								</a>
+							</li><!--.android-->
+						</ul><!--.applications-->
+						<div class="mail-poet-wrapper">
+							<div class="form-wrapper">
+								<form action="" method="post">
+									<input type="text" name="name" value="" placeholder="نام کامل">
+									<input type="text" name="email" value="" placeholder="ایمیل">
+									<input type="submit" value="عضویت در خبرنامه">
+								</form>
+							</div>
+						</div><!--.mail-poet-wrapper-->
+					</div><!--.footer-widg-->
+				</div><!--.row-->
+			</div><!--.container-->
+		</section>
+        <section class="copyright">
+            <div class="container">
+                <p><i class="fa fa-copyright"></i> International Relations Think Tank(www.irthink.com) All Right Reserved 2004-2019 </p>
+            </div>
+        </section><!--.copyright-->
+	</footer><!--#footer-->
+    <script src="assets/js/jquery-3.3.1.min.js"></script>
+    <script src="assets/js/slick.js"></script>
+    <script src="assets/js/main.js"></script>
+</body>
+</html>
